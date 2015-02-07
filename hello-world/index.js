18d899b1964fe9b144fe5e5db5d165d130e84607
ced404 <ced@404err.com>
2015-02-07 10:36:15 +0100

    Update index.js

---
+++ hello-world/index.js
@@ -0,0 +1,43 @@
+
+simply.fullscreen(true);
+
+simply.title('Hello World!');
+simply.vibe('short');
+
+
+function geolocate () {
+  navigator.geolocation.getCurrentPosition(function(pos) {
+    var coords = pos.coords;
+    var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+        'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+    ajax({ url: weatherUrl, type: 'json' }, function(data) {
+      simply.text({ title: data.name, subtitle: data.main.temp });
+    });
+  });
+}
+
+
+
+// click
+simply.on('singleClick', function(e) {
+  
+  simply.text({ title: 'singleClick', 
+                subtitle: 'You pressed the ' + e.button + ' button!' });
+  
+});
+
+// long click
+simply.on('longClick', function(e) {
+  
+  // simply.subtitle('You held the ' + e.button + ' button!');
+  
+  if( e.button == 'select' ) {
+    geolocate();
+  }
+  
+});
+
+simply.on('accelTap', function(e) {
+  simply.text({ title: 'accelTap' ,
+                subtitle: 'You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!' });
+});
